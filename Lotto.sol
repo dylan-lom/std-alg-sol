b19d509032d610daf30ebbbaa99cda1696a8cc4b
dylan-lom <39334387+dylan-lom@users.noreply.github.com>
2019-02-18 10:25:36 +1100

    Create Lotto.sol

---
+++ Lotto.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.1;
+
+contract Lotto {
+    uint ts;
+    uint[6] results;
+    function random(uint range) public view returns (uint8) {
+        return uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%range);
+    }
+}
