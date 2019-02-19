f8a448037ddc727997ca2410a920db63da2411bb
dylan-lom <39334387+dylan-lom@users.noreply.github.com>
2019-02-19 14:00:07 +1100

    + drawLotto()
    solved problem of only generating one number (due to pseudo-randomness caused by timestamp), by added "nonce" value, which increments for each draw

--- Lotto.sol
+++ Lotto.sol
@@ -1,13 +1,24 @@
 pragma solidity ^0.5.1;
 
 contract Lotto {
-    uint ts;
+    uint nonce;
     uint[6] results;
     function random(uint range) public view returns (uint8) {
-        return uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%range);
+        return uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, nonce)))%range);
     }
     
     function getTimestamp() public view returns (uint) {
         return block.timestamp;
+    }
+    function getDifficulty() public view returns (uint) {
+        return block.difficulty;
+    }
+    
+    function drawLotto() public returns (uint[6] memory){
+        for (uint i=0; i<6; i++){
+            results[i] = random(100);
+            nonce++;
+        }
+        return results;
     }
 }
